061b8c77f1a8389df2d8eee801f74fd75ede00e3
JPTF <jptorresflorez@gmail.com>
2021-06-02 21:27:00 -0300

    actualizar servicio con PUT

--- controllers/servicosController.js
+++ controllers/servicosController.js
@@ -23,7 +23,7 @@ const servicosController = {
         let {nome, descricao, preco} = request.body;
 
         /** pega el nombre del archivo (upload) */
-        let ilustracao = request.file.path;
+        let ilustracao = request.file.filename;
         
         /* adiciono el nuevo servicio al array*/
         servicos.push({id: uuid(), nome, descricao, preco, ilustracao });
@@ -37,13 +37,44 @@ const servicosController = {
         /* redirecciona para lista de servicios */
         return response.redirect('/admin/servicos')
     },
-    show: (req, res) => {
-        // console.log(req.params);
-        // pegando parametro nome de rota /servico/:nome
-        const {nome} = req.params
+    editar: (request, response) => {
+        /** pegando parametro id da URL */
+        let{id} = request.params;
+        /** busca srevicio por el id */
+        let servicoEncontrado = servicos.find(servico => servico.id == id)
+        /** renderiza el view y manda tituloy obj del servicio */
+        return response.render('servicosEditar',  { titulo: 'Editar Serviços', servico: servicoEncontrado})
+    },
+    atualizar: (request, response) => {
+        /** pegando parametro id da URL */
+        let{id} = request.params;
 
-        return res.send(`exibindo detalhes do servico ${nome}`);
+        /** pegando informaciones del formulario */
+        let { nome, descricao, preco} = request.body
+
+        /** busca srevicio por el id */
+        let servicoEncontrado = servicos.find(servico => servico.id == id);
+
+        /** atribuir los nuevos valores a servicoEncontrado */
+        servicoEncontrado.nome = nome;
+        servicoEncontrado.descricao = descricao;
+        servicoEncontrado.preco = preco;
+
+        /** verifica si hay una nueva imagen antes de atribuir */
+        if(request.file){
+            servicoEncontrado.ilustracao = request.file.filename;
+        }
+        
+        /* convierte el array para json*/
+        let dadosJson = JSON.stringify(servicos);
+
+        /*salva json actualizado en el archivo*/
+        fs.writeFileSync(servicosPath, dadosJson)
+
+        /* redirecciona para lista de servicios */
+        return response.redirect('/admin/servicos')
     }
+    
 }
 
 module.exports = servicosController;
